93b47ccf3200c77e5d38d848ef3e5e8155b6c28a
Alex Ling <hkalexling@gmail.com>
2017-05-11 22:57:07 +0800

    Initial Commit

---
+++ AirFox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AirFox
+//
+//  Created by Alex Ling on 11/5/2017.
+//  Copyright © 2017 Alex Ling. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AirFox/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  AirFox
+//
+//  Created by Alex Ling on 11/5/2017.
+//  Copyright © 2017 Alex Ling. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
