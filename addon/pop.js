19ac3f3e6490bbce6803045fddee321d3b8983d7
Alex Ling <hkalexling@gmail.com>
2017-05-14 03:01:31 +0800

    -

--- addon/pop.js
+++ addon/pop.js
@@ -1,18 +1,16 @@
-browser.tabs.query({active: true}).then(tabs => {
-		
-	const filtered = tabs.filter((tab) => {
-		return typeof tab.url !== 'undefined'; 
+browser.windows.getLastFocused({populate: true}).then(info => {
+	const tabs = info.tabs;
+	const filtered = tabs.filter(tab => {
+		return tab.active;
 	});
-
-	if (filtered.length !== 1) return;
-
 	const URL = filtered[0].url;
+	console.log(URL);
 	browser.storage.local.get('port').then(obj => {	
 		const port = obj.port || 8964;
 
 		fetch(`http://localhost:${port}/?url=${URL}`).then(res => {
 			if (res.status !== 200) {
-				console.log(`status code: ${res.status}`);
+				console.error(`status code: ${res.status}`);
 			}
 			window.close();
 		}).catch(err => {
@@ -21,9 +19,9 @@ browser.tabs.query({active: true}).then(tabs => {
 			document.querySelector('#message').innerHTML = '<h3>Error</h3><p>AirFox failed to connect to the main AirFox macOS app.</p><p>Please make sure that the AirFox app is running and the Addon\'s port number is configured corrected.</p><p>You can configure the port number at   <code>about:addones --> AirFox --> Preferences</code></p>';
 		});	
 	}, err => {
-		console.log(`error: ${err}`);
+		console.error(`error: ${err}`);
 	});
 
 }, err => {
-	console.log(err);
+	console.error(err);
 });
